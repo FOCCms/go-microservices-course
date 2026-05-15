3bae787f742f5e9544d617780e2092d2f8f45a25
Artem <mihocc18@gmail.com>
2026-05-15 21:09:51 +0300

    Исправляет api тесты

--- order/pkg/app/app.go
+++ order/pkg/app/app.go
@@ -1,19 +1,16 @@
 package app
 
 import (
-	"log/slog"
-	"os"
+	"context"
 
-	"github.com/IBM/sarama"
+	"github.com/FOCCms/go-microservices-course/order/internal/model"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	orderV1API "github.com/FOCCms/go-microservices-course/order/internal/api/order/v1"
 	invetntoryV1Client "github.com/FOCCms/go-microservices-course/order/internal/client/grpc/inventory/v1"
 	paymentV1Client "github.com/FOCCms/go-microservices-course/order/internal/client/grpc/payment/v1"
-	orderProducer "github.com/FOCCms/go-microservices-course/order/internal/producer/order_producer"
 	orderRepository "github.com/FOCCms/go-microservices-course/order/internal/repository/order"
 	orderService "github.com/FOCCms/go-microservices-course/order/internal/service/order"
-	wrappedKafkaProducer "github.com/FOCCms/go-microservices-course/platform/pkg/kafka/producer"
 	orderv1 "github.com/FOCCms/go-microservices-course/shared/pkg/openapi/order/v1"
 	inventoryv1 "github.com/FOCCms/go-microservices-course/shared/pkg/proto/inventory/v1"
 	paymentv1 "github.com/FOCCms/go-microservices-course/shared/pkg/proto/payment/v1"
@@ -25,7 +22,7 @@ func NewHTTPHandler(pool *pgxpool.Pool, txManager orderService.TxManager, invent
 	pc := paymentV1Client.New(paymentClient)
 	ic := invetntoryV1Client.New(inventoryClient)
 
-	orderProducerService := initProducer()
+	orderProducerService := NewNoopProducer()
 
 	service := orderService.NewService(orderRepo, pc, ic, txManager, orderProducerService)
 
@@ -47,25 +44,12 @@ func NewHTTPHandlerWithProducer(pool *pgxpool.Pool, txManager orderService.TxMan
 	return orderV1API.SetupServer(api)
 }
 
-func initProducer() orderService.OrderProducerService {
-	p, err := sarama.NewSyncProducer(
-		[]string{"localhost:9092"},
-		saramaConfig(),
-	)
-	if err != nil {
-		slog.Error("initProducer", "error", err)
-		os.Exit(1)
-	}
-	wrapped := wrappedKafkaProducer.NewProducer(p, "test-topic")
+type noopProducer struct{}
 
-	srv := orderProducer.NewService(wrapped)
-	return srv
+func (n *noopProducer) ProduceOrderPaid(ctx context.Context, event model.OrderPaidEvent) error {
+	return nil
 }
 
-func saramaConfig() *sarama.Config {
-	cfg := sarama.NewConfig()
-	cfg.Version = sarama.V4_0_0_0
-	cfg.Producer.Return.Successes = true
-
-	return cfg
+func NewNoopProducer() *noopProducer {
+	return &noopProducer{}
 }
