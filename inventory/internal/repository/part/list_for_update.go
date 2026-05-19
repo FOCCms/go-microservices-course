6039a94e27296f83babb9846d7ab6b9d13825d20
Artem <mihocc18@gmail.com>
2026-05-19 18:36:28 +0300

    Убирает неиспользуемую часть из list_for_update

--- inventory/internal/repository/part/list_for_update.go
+++ inventory/internal/repository/part/list_for_update.go
@@ -14,15 +14,16 @@ import (
 )
 
 func (r *repository) ListForUpdate(ctx context.Context, filter record.PartFilter) ([]model.Part, error) {
+	if len(filter.UUIDs) == 0 {
+		return nil, fmt.Errorf("считать детали по списку uuid: %w", errs.ErrPartNotFound)
+	}
+
 	// сортировка перед for update
 	sort.Slice(filter.UUIDs, func(i, j int) bool {
 		return filter.UUIDs[i] < filter.UUIDs[j]
 	})
 
-	var rows pgx.Rows
-	var err error
-	if len(filter.UUIDs) > 0 {
-		const query = `
+	const query = `
 				SELECT p.uuid, p.name, p.description, p.part_type, p.price, p.stock_quantity, p.properties, p.reserved, p.created_at
 		    FROM parts p
 		    JOIN unnest($1::uuid[]) WITH ORDINALITY AS input_uuids(uuid, order_num)
@@ -30,18 +31,7 @@ func (r *repository) ListForUpdate(ctx context.Context, filter record.PartFilter
 		    ORDER BY input_uuids.order_num
 			FOR UPDATE
 		`
-		rows, err = r.getter.DefaultTrOrDB(ctx, r.pool).Query(ctx, query, filter.UUIDs)
-	} else {
-		const query = `
-			SELECT uuid, name, description, part_type, price, stock_quantity, properties, reserved, created_at
-			FROM parts
-			WHERE $1 = 'UNSPECIFIED' OR part_type = $1
-			ORDER BY name ASC
-			FOR UPDATE
-		`
-		rows, err = r.getter.DefaultTrOrDB(ctx, r.pool).Query(ctx, query, filter.PartType)
-	}
-
+	rows, err := r.getter.DefaultTrOrDB(ctx, r.pool).Query(ctx, query, filter.UUIDs)
 	if err != nil {
 		return nil, fmt.Errorf("считать детали: %w", err)
 	}
