e4da1b16bdf9097dc0637bfaa684b3b16f399477
Artem <mihocc18@gmail.com>
2026-05-25 13:43:56 +0300

    Добавляет файл миграции и обновляет платформу

---
+++ migrations/iam/20260521074502_create_users_table.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+                       uuid UUID PRIMARY KEY,
+                       login VARCHAR(255) NOT NULL UNIQUE,
+                       password_hash VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+                       updated_at TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
